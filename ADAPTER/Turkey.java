554075a7dc9f40819759da58111d466d0ca704eb
Feodor08 <97873714+Feodor08@users.noreply.github.com>
2023-02-18 09:57:40 +0700

    Add files via upload

---
+++ ADAPTER/Turkey.java
@@ -0,0 +1,17 @@
+pudlic class TurkeyAdapter implements Duck {
+	Turkey turkey;
+
+	public TurkeyAdapter(Turkey turkey) {
+		this.turkey = turkey;
+	}
+
+	public void quack() {
+		turkey.gobble();
+	}
+
+	public void fly() {
+		for(int i=0; i < 5; i++) {
+			turkey.fly();
+		}
+	}
+}
